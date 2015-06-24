4aabd415ac28cd8735e9ccec7c5b9c179bdd7293
Rachel Black <Rachel@Rachels-MacBook-Pro.local>
2015-06-24 19:13:18 -0400

    started angry boss program

---
+++ week3.rb
@@ -0,0 +1,5 @@
+puts "Boss, I want a raise."
+puts "A raise? Ha why would I give you a raise?".upcase
+
+puts "I need to take next Wednesday off"
+puts "You can take the rest of the week off, your're fired!".upcase
