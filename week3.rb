88dc39b819fbc1993e1d48b8610610b4a68bf830
Rachel Black <Rachel@Rachels-MacBook-Pro.local>
2015-06-24 19:21:19 -0400

    added variable to angry boss

--- week3.rb
+++ week3.rb
@@ -1,5 +1,9 @@
 puts "Boss, I want a raise."
 puts "A raise? Ha why would I give you a raise?".upcase
 
-puts "I need to take next Wednesday off"
+puts "I need to take next Wednesday off."
 puts "You can take the rest of the week off, your're fired!".upcase
+
+puts "Can I work from home?"
+fired = "You're fired"
+puts fired.upcase
